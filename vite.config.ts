6b070d4b4b5dde75050e9a0634ca5c3341b31445
TuThanh11 <Caottu1980@gmail.com>
2026-03-21 16:45:52 +0700

    fix: lỗi ko kết nối được BE và thêm công khai link chơi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/ws-game': {
+        target: 'ws://localhost:8080',
+        ws: true
+      }
+    }
+  }
 })
